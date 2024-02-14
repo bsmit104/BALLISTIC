257ad016db23eb4635dd8acbda9dfb86a9d518a8
Anthony Umemoto <atumemot@ucsc.edu>
2024-02-13 16:40:06 -0800

    level manager skeleton

---
+++ BALLISTIC/Assets/Networked/NetworkedScripts/NetworkLevelManager.cs
@@ -0,0 +1,322 @@
+using System.Collections;
+using System.Collections.Generic;
+using Fusion;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Manages scene transitions between levels.
+/// Tracks how many players are alive to know when to end the game, and then move on to the next level.
+/// </summary>
+public class NetworkLevelManager : MonoBehaviour
+{
+    /// <summary>
+    /// Get the global level manager instance.
+    /// </summary>
+    public static NetworkLevelManager Instance { get { return _instance; } }
+    private static NetworkLevelManager _instance = null;
+
+    private NetworkPlayerManager playerManager;
+    // private NetworkBallManager ballManager;
+
+    /// <summary>
+    /// Get the local network runner instance.
+    /// </summary>
+    public NetworkRunner Runner { get { return _runner; } }
+    private NetworkRunner _runner = null;
+
+    [Header("Scene Indices")]
+    [Tooltip("The build index of the lobby scene")]
+    [SerializeField] private int lobbySceneIndex;
+    [Tooltip("The next level will be picked based on a range of scene build indices. Each level should be placed in a row.")]
+    [SerializeField] private int firstLevelIndex;
+    [Tooltip("The next level will be picked based on a range of scene build indices. Each level should be placed in a row.")]
+    [SerializeField] private int lastLevelIndex;
+
+    /// <summary>
+    /// Initializes the level manager, should only be called once when the NetworkRunnerPrefab is created.
+    /// </summary>
+    /// <param name="runner">The local NetworkRunner.</param>
+    public void Init(NetworkRunner runner, NetworkPlayerManager players) // TODO: add in ball manager
+    {
+        if (_instance != null && _instance != this)
+        {
+            Debug.LogError("NetworkLevelManager singleton instantiated twice");
+            Destroy(this);
+        }
+        _instance = this;
+        _runner = runner;
+        playerManager = players;
+        // ballManager = balls;
+
+        numLevels = lastLevelIndex - firstLevelIndex;
+        remainingLevels = new List<int>(numLevels);
+    }
+
+    /// <summary>
+    /// Returns true if the current scene loaded in the lobby waiting room.
+    /// </summary>
+    public bool IsAtLobby { get { 
+        return SceneManager.GetActiveScene().buildIndex == lobbySceneIndex;
+    } }
+
+    // * Level Picking ==========================================
+
+    [Space]
+    [Header("Level Picking")]
+    [Tooltip("The level manager will track which levels have been played. Refreshing will reset this list.")]
+    [Range(0f, 1f)]
+    [SerializeField] private float refreshChance;
+
+    // the number of levels to pick from
+    private int numLevels;
+
+    // an array of levels that haven't been played yet
+    private List<int> remainingLevels;
+
+    // reset the remaining levels
+    private void Refresh()
+    {
+        remainingLevels.Clear();
+        for (int i = 0; i < numLevels; i++) 
+        {
+            remainingLevels.Add(firstLevelIndex + i);
+        }
+    }
+
+    /// <summary>
+    /// Gets a random scene build index for a level.
+    /// Avoids revisiting levels, which can be controlled with the "refreshChance" attribute.
+    /// </summary>
+    /// <returns>A scene build index.</returns>
+    public int GetRandomLevel()
+    {
+        if (remainingLevels.Count == 0 || Random.Range(0f, 1f) < refreshChance)
+        {
+            Refresh();
+        }
+
+        int index = Random.Range(0, remainingLevels.Count);
+        int selection = remainingLevels[index];
+        remainingLevels.RemoveAt(index);
+
+        return selection;
+    }
+
+    // * =========================================================
+
+    // *Transitions ==============================================
+
+    [Space]
+    [Header("Level Transitions")]
+    [Tooltip("Duration for transition out of a scene.")]
+    [SerializeField] float exitTransitionDuration;
+    [Tooltip("Duration for transition into a scene.")]
+    [SerializeField] float enterTransitionDuration;
+
+    // exit transition ============
+
+    private Coroutine exitTransition;
+    private bool exitRunning = false;
+    public bool ExitRunning { get { return exitRunning; } }
+
+    public void StartExitTransition()
+    {
+        if (exitTransition == null)
+        {
+            StopCoroutine(exitTransition);
+        }
+        exitRunning = true;
+        exitTransition = StartCoroutine(ExitTransition());
+    }
+
+    // exit tween (probably some sort of fade to black) to hide scene unloading / game object moving
+    IEnumerator ExitTransition()
+    {
+        float timer = exitTransitionDuration;
+
+        while (timer > 0)
+        {
+            timer -= Time.deltaTime;
+
+            // TODO: create exit transition animation
+
+            yield return null;
+        }
+        exitRunning = false;
+    }
+
+    // ============================
+
+    // enter transition ===========
+
+    private Coroutine enterTransition;
+    private bool enterRunning = false;
+    public bool EnterRunning { get { return enterRunning; } }
+
+    public void StartEnterTransition()
+    {
+        if (enterTransition == null)
+        {
+            StopCoroutine(enterTransition);
+        }
+        enterRunning = true;
+        enterTransition = StartCoroutine(EnterTransition());
+    }
+
+    // enter tween (probably a fade in from black) to cleaning transition from a loading state into the gameplay
+    IEnumerator EnterTransition()
+    {
+        float timer = enterTransitionDuration;
+
+        while (timer > 0)
+        {
+            timer -= Time.deltaTime;
+
+            // TODO: create enter transition animation
+
+            yield return null;
+        }
+        enterRunning = false;
+    }
+
+    // ============================
+
+    // full transition ============
+
+    private Coroutine fullTransition;
+    private bool levelChangeRunning = false;
+    public bool LevelChangeRunning { get { return levelChangeRunning; } }
+
+    private void StartLevelTransition(int buildIndex)
+    {
+        if (fullTransition != null)
+        {
+            StopCoroutine(fullTransition);
+        }
+        levelChangeRunning = true;
+        fullTransition = StartCoroutine(LevelTransition(buildIndex));
+    }
+
+    // exit transition, load next level, enter transition
+    IEnumerator LevelTransition(int buildIndex)
+    {
+        StartExitTransition();
+        while (ExitRunning)
+        {
+            yield return null;
+        }
+
+        Runner.LoadScene(SceneRef.FromIndex(buildIndex));
+        ResetLevel();
+
+        StartEnterTransition();
+        while (EnterRunning)
+        {
+            yield return null;
+        }
+        levelChangeRunning = false;
+    }
+
+    /// <summary>
+    /// Synchronizes scene transitions between clients.
+    /// Activates transition animations for unloading and loading the scene.
+    /// Will only execute on the host instance.
+    /// </summary>
+    /// <param name="buildIndex">The build index for the level.</param>
+    public void GoToLevel(int buildIndex)
+    {
+        if (!Runner.IsServer) return;
+
+        if (buildIndex != lobbySceneIndex && (buildIndex < firstLevelIndex || lastLevelIndex < buildIndex))
+        {
+            Debug.LogError("Invalid scene index used in NetworkLevelManager.GoToLevel(): " + buildIndex.ToString());
+            return;
+        }
+
+        StartLevelTransition(buildIndex);
+    }
+
+    // ============================
+
+    // * =========================================================
+
+    // * Level Prep ==============================================
+
+    /// <summary>
+    /// Resets the current level to be prepared for play.
+    /// Places players and balls into scene.
+    /// </summary>
+    public void ResetLevel()
+    {
+        // TODO: implement placements using Spawner.GetSpawnPosition()
+    }
+
+    // * =========================================================
+
+    // * Win State ===============================================
+
+    [Space]
+    [Header("Win Screen")]
+    [Tooltip("How long the winner will be displayed before going to the next level.")]
+    [SerializeField] float winScreenDuration;
+
+    private PlayerRef winner;
+
+    private Coroutine winnerSequence;
+    private bool winSequenceRunning = false;
+    public bool WinSequenceRunning { get { return winSequenceRunning; } }
+
+    /// <summary>
+    /// Set a winner player, and transition to the winning screen.
+    /// Should be called by player manager when only 1 player is left alive.
+    /// </summary>
+    /// <param name="player">The winning player</param>
+    public void DeclareWinner(PlayerRef player)
+    {
+        winner = player;
+
+        if (winnerSequence != null)
+        {
+            StopCoroutine(winnerSequence);
+        }
+        winSequenceRunning = true;
+        StartCoroutine(WinnerSequence());
+    }
+
+    private IEnumerator WinnerSequence()
+    {
+        StartExitTransition();
+        while (ExitRunning)
+        {
+            yield return null;
+        }
+
+        // TODO: Set up winner screen
+
+        StartEnterTransition();
+        while (EnterRunning)
+        {
+            yield return null;
+        }
+
+        winSequenceRunning = false;
+
+        float timer = winScreenDuration;
+        while (timer > 0)
+        {
+            timer -= Time.deltaTime;
+
+            // something can go here or not...
+
+            yield return null;
+        }
+
+        winSequenceRunning = false;
+
+        // go to the next level
+        GoToLevel(GetRandomLevel());
+    }
+
+    // * =========================================================
+}
