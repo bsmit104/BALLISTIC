b0a1bd4e80f6ce3a5c36a53484df4c6dcde88384
Anthony Umemoto <atumemot@ucsc.edu>
2024-03-13 18:10:30 -0700

    misc scripts comments

--- BALLISTIC/Assets/Scripts/Menus/ComplexButton.cs
+++ BALLISTIC/Assets/Scripts/Menus/ComplexButton.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Used for buttons that have multiple images that should change color with 
+/// hover and click events.
+/// </summary>
 public class ComplexButton : MonoBehaviour
 {
+    [Tooltip("The color listeners will be multiplied with on hover.")]
     public Color onHoverColor;
+    [Tooltip("The color listeners will be multiplied with on click.")]
     public Color onClickColor;
 
+    [Tooltip("The UI elements that should be colored.")]
     [SerializeField] private Image[] listeners;
+    // Cache the original colors for each listener for exit events
     private Color[] listenerOriginalColors;
 
     private bool isHovering = false;
@@ -16,6 +24,7 @@ public class ComplexButton : MonoBehaviour
 
     void Awake()
     {
+        // Cache original colors
         listenerOriginalColors = new Color[listeners.Length];
         for (int i = 0; i < listeners.Length; i++)
         {
@@ -56,6 +65,7 @@ public class ComplexButton : MonoBehaviour
         isClicking = false;
         if (isHovering)
         {
+            // Set colors to on hover color if mouse is still on the button
             for (int i = 0; i < listeners.Length; i++)
             {
                 listeners[i].color = listenerOriginalColors[i] * onHoverColor;
--- BALLISTIC/Assets/Scripts/Menus/LoadingPopup.cs
+++ BALLISTIC/Assets/Scripts/Menus/LoadingPopup.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
+/// <summary>
+/// Instantiated to show players the game is currently loading something.
+/// Only instantiate this if loading consistently takes a long time, and it will result in a scene change.
+/// </summary>
 public class LoadingPopup : MonoBehaviour
 {
+    [Tooltip("Displays the animated loading text.")]
     [SerializeField] private TextMeshProUGUI text;
+    [Tooltip("The duration between dots '.' being added to text.")]
     [SerializeField] private float dotDuration;
+    [Tooltip("The max number of dots '.' that will be added before looping back to 0.")]
     [SerializeField] private int dotCount;
 
     private float timer = 0;
@@ -19,6 +26,7 @@ public class LoadingPopup : MonoBehaviour
 
     void Update()
     {
+        // Add the dots "." to the loading string to create an animated loading effect
         if (timer <= 0)
         {
             text.text += " .";
